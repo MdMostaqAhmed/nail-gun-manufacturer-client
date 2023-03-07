b5fa45b9238277ac8f14a8ce440f2a222b2c7c3a
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-03-07 11:13:10 +0600

    add Tools, BussinessSummery, Review and Footer Components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import BusinessSummery from './BusinessSummery';
 import Carousel from './Carousel';
+import Footer from './Footer';
+import Review from './Review';
+import Tools from './tools';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
             <Carousel></Carousel>
+            <Tools></Tools>
+            <BusinessSummery></BusinessSummery>
+            <Review></Review>
+            <Footer></Footer>
         </div>
     );
 };
