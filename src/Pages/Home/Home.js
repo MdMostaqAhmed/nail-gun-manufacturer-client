9ce6d69a143a323f255ec022e655ce8b7cd02a23
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-03-07 09:38:26 +0600

    add carousel to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Carousel from './Carousel';
 
 const Home = () => {
     return (
         <div>
             <h1>This is home</h1>
+            <Carousel></Carousel>
         </div>
     );
 };
