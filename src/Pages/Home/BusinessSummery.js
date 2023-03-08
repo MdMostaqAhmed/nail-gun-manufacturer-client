caa9b0e18ee4cf6be4b8ccebb3bac1a48c292503
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-03-08 08:29:54 +0600

    install React Font Awesome

--- src/Pages/Home/BusinessSummery.js
+++ src/Pages/Home/BusinessSummery.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const BusinessSummery = () => {
     return (
-        <div>
-            <h2>Our Business Summery</h2>
+        <div className='container mx-auto'>
+            <h2 className='text-center text-3xl font-bold mt-5'>Our Business Summery</h2>
+
+
+
+
         </div>
     );
 };
