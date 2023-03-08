df61f4a9936cec3f902b4bbefa86bce9b34e17c0
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-03-08 10:41:29 +0600

    footer added

--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Review = () => {
     return (
-        <div>
-            <h2>Customer Review</h2>
+        <div className='container mx-auto mt-10 mb-8'>
+            <h2 className='text-center text-5xl font-bold'>Customer Review</h2>
         </div>
     );
 };
