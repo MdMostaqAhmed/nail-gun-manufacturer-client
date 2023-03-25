a6988a955bc12a416e2ce665212424ddf469935c
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-03-25 10:33:39 +0600

    use custom hook to Pruducts page and load data

--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Product = ({ product }) => {
-    const { name, img } = product;
+    const { name, img, description } = product;
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                    <img src="img" alt="Shoes" className="rounded-xl" />
+                    <img src={img} alt="Shoes" className="rounded-xl" />
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>{description.slice(0, 120) + "..."}</p>
                     <div className="card-actions">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
