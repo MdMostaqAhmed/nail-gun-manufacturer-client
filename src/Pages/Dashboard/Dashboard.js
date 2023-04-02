8826b23bc3550d58403d2080f01c4747bdebd48c
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-04-02 14:27:24 +0600

    show user info in dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,29 +3,97 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import auth from '../../firebase.init';
 import useAdmin from '../../Hooks/useAdmin';
+import CustomLink from '../Shared/CustomLink';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Dashboard = () => {
     const [user] = useAuthState(auth);
     const [admin, setAdmin] = useAdmin(user);
     return (
-        <div className="drawer drawer-mobile">
-            <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content ">
-                <h2 className='text-3xl font-bold'>Dashboard</h2>
-                <Outlet></Outlet>
-
-
+        <>
+            <div className="navbar-end  lg:hidden">
+                <label
+                    tabIndex="1"
+                    htmlFor="my-drawer-2"
+                    className="btn btn-accent lg:hidden"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h8m-8 6h16"
+                        />
+                    </svg>
+                </label>
             </div>
-            <div className="drawer-side">
-                <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-                    <li><Link to="/dashboard">My Profile</Link></li>
-                    <li><Link to="/dashboard/myOrders">My Orders</Link></li>
-                    <li><Link to="/dashboard/addReview">Add A Review</Link></li>
-                </ul>
+            <div className="drawer drawer-mobile">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content ">
+                    {/* <!-- Page content here --> */}
+                    <h2
 
+                        className="text-3xl font-semibold text-primary text-center my-3"
+                    >
+                        Welcome to Your Dashboard
+                    </h2>
+                    <Outlet></Outlet>
+                </div>
+                <div className="drawer-side lg:h-auto">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-48 lg:w-48 bg-primary  text-neutral ">
+                        {/* <!-- Sidebar content here --> */}
+                        <li className="text-xl font-semibold text-warning">
+                            {admin ? "Admin" : "User"}
+                        </li>
+                        <li>
+                            <CustomLink to="/dashboard">My Profile</CustomLink>
+                        </li>
+                        {!admin && (
+                            <>
+                                <li>
+                                    <CustomLink to="/dashboard/myOrders">My Orders</CustomLink>
+                                </li>
+                                <li>
+                                    <CustomLink to="/dashboard/myReview">Add Review</CustomLink>
+                                </li>
+                            </>
+                        )}
+                        {admin && (
+                            <>
+                                <li>
+                                    <CustomLink to="/dashboard/manageUsers">
+                                        Manage Users
+                                    </CustomLink>
+                                </li>
+                                <li>
+                                    <CustomLink to="/dashboard/addProduct">
+                                        Add Product
+                                    </CustomLink>
+                                </li>
+                                <li>
+                                    <CustomLink to="/dashboard/manageOrders">
+                                        Manage Orders
+                                    </CustomLink>
+                                </li>
+                                <li>
+                                    <CustomLink to="/dashboard/manageProducts">
+                                        Manage Products
+                                    </CustomLink>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../Hooks/useAdmin';
+import "./MyProfile.css";
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+    console.log(user.email)
+    console.log(user.photoURL)
+    const [admin, setAdmin] = useAdmin(user);
     return (
-        <div>
-            <h2>My Profile</h2>
+        <div >
+            <div className="card-containers bg-gradient-to-t from-orange-500 to-yellow-500">
+                <span className="pro text-[#B3B8CD] bg-primary rounded-full btn-sm ">
+                    {admin ? "admin" : "User"}
+                </span>
+                <img
+                    className='mx-auto mt-3'
+                    src={user?.photoURL}
+                    alt="user"
+                />
+                <h3 className="text-2xl my-3 font-bold text-primary">
+                    {user?.displayName}
+                </h3>
+
+                <div
+
+                    className="buttons"
+                >
+                    <Link to="/dashboard" className="btn btn-primary mr-3">
+                        About Me
+                    </Link>
+
+                    <Link to="/dashboard/myOrders" className="btn btn-accent mr-3">
+                        Update Profile
+                    </Link>
+                    <Link to="/dashboard/addReview" className="btn btn-secondary">
+                        Add More Info
+                    </Link>
+                </div>
+                <div className="skills">
+                    <Outlet></Outlet>
+                </div>
+            </div>
         </div>
     );
 };
