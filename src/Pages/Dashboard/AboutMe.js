e492b5fc1ea61f2bac3839974cfad6153468de06
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-04-02 15:50:35 +0600

    load orders from server to AboutMe component

--- src/Pages/Dashboard/AboutMe.js
+++ src/Pages/Dashboard/AboutMe.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import useAdmin from '../../Hooks/useAdmin';
+import useUserOrders from '../../Hooks/useUserOrders';
 
 const AboutMe = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useUserOrders([]);
+    const [admin, setAdmin] = useAdmin(user);
+    const email = user?.email;
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/user", {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => setUsers(data));
+    }, []);
+    const exactUser = users.find((user) => user.email === email);
+    const userOrders = orders.filter((order) => order.userEmail === email);
     return (
         <div>
-            <h1>About Me</h1>
+            <h3>
+                Name : <span className="font-semibold"> {user?.displayName}</span>
+            </h3>
+            <h3>
+                Email : <span className="font-semibold">{user?.email}</span>
+            </h3>
+            {!admin && (
+                <h3>
+                    Total Order Placed:
+                    <span className="font-semibold">{userOrders?.length}</span>
+                </h3>
+            )}
+            <ul className="grid grid-cols-3 gap-3">
+                <li>Status: {exactUser?.role}</li>
+                <li>Location: {exactUser?.location || "Not Found"}</li>
+                <li>Birthday: {exactUser?.date || "Not Found"}</li>
+                <li>School: {exactUser?.school || "Not Found"}</li>
+                <li>Phone: {exactUser?.phone || "Not Found"}</li>
+                <li>GitHub: {exactUser?.Github || "Not Found"}</li>
+                <li>LinkedIn: {exactUser?.linkedin || "Not Found"}</li>
+            </ul>
         </div>
     );
 };
