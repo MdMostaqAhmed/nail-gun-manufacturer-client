79dd25311a50b62227c7118226708d1018dd55b2
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-04-02 15:36:17 +0600

    load orders from server to useUserOrders custom hook

--- src/Hooks/useUserOrders.js
+++ src/Hooks/useUserOrders.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import { signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../firebase.init';
 
 const useUserOrders = () => {
-    return (
-        <div>
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
-        </div>
-    );
+    useEffect(() => {
+        if (user) {
+            fetch(
+                `http://localhost:5000/orders?userEmail=${user.email}`,
+                {
+                    method: "GET",
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+                    },
+                }
+            )
+                .then((res) => {
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem("accessToken");
+                        //Home
+                        navigate("/");
+                    }
+                    return res.json();
+                })
+                .then((data) => setOrders(data));
+        }
+    }, [user, orders]);
+    return [orders, setOrders];
 };
 
 export default useUserOrders;
