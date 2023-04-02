f6a6d90b9f8a16d15870f84753d8392e1ca4d3dc
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2023-04-02 14:37:30 +0600

    create UpdateProfile and AddMoreInfo component

---
+++ src/Pages/Dashboard/AddMoreInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddMoreInfo = () => {
+    return (
+        <div>
+            <h1>Add More Info</h1>
+        </div>
+    );
+};
+
+export default AddMoreInfo;
